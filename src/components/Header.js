ea38c872217f45e917732376f7727d9c73bc1a08
sheniatung <stung@appneta.com>
2022-06-05 13:30:11 -0700

    routing, backend server

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,27 @@
 import PropTypes from 'prop-types';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Button from './Button';
 
 function Header({ title, onAdd, showAdd }) {
   // can also use event object (e) =>
 
+  const location = useLocation();
+  const navigate = useNavigate();
+
   return (
     <header className="header">
       {/* <h1 style={headingStyle}>{title}</h1> */}
       <h1>{title}</h1>
-      <Button
+      {location.pathname === '/' ? <Button
         color={showAdd ? 'red' : 'green'}
         text={showAdd ? 'Close' : 'Add'}
         onClick={onAdd}
-      />
+      /> : <Button 
+        onClick={() => {
+          navigate('/');
+        }}
+        text='Go Back'
+      />}
     </header>
   );
 }
